9d6bf582a91780448ffb108224009280fd354ba8
steven03.chen <steven03.chen@vipshop.com>
2016-09-18 16:40:13 +0800

    auto push

--- examples/test/tests/main.js
+++
@@ -1,9 +0,0 @@
-
-//require('./request.vue')
-export default {
-	options:require('./options.vue'),
-	//promise:require('./promise.vue'),
-	request:require('./request.vue'),
-	//cookie:require('./cookie.vue'),
-	//function:require('./function.vue'),
-}
--- tunk-vue.js
+++ tunk-vue.js
@@ -8,9 +8,9 @@
 
 
         tunk.hook('updateComponentState', function(origin){
-            return function(targetObject, stateName, newValue, action){
+            return function(targetObject, stateName, newValue, module, action){
                 if (targetObject.$options.beforeStateInject)
-                    targetObject.$options.beforeStateInject.call(targetObject, stateName, newValue, action);
+                    targetObject.$options.beforeStateInject.call(targetObject, stateName, newValue, module+'.'+action);
                 targetObject.$set(stateName, newValue);
                 //origin.call(null, targetObject, stateName, newValue, action);
             }
@@ -26,51 +26,39 @@
 
                     for (var x in stateOptions) if (stateOptions.hasOwnProperty(x)) {
                         if (typeof stateOptions[x] === 'string' && stateOptions[x].indexOf('.') > -1) {
-                            stateOptions_[x] = stateOptions[x].split('.');
+                            Vue.util.defineReactive(this, x, tunk.connection.state(this, x, stateOptions[x].split('.')));
                         } else {
-                            throw 'the path of state should had dot separator: ' + x + ':' + stateOptions[x];
+                            throw 'the path of state should had dot separator ' + x + ':' + stateOptions[x];
                         }
                     }
-
-                    this.$options.stateOptions_=stateOptions_;
-
-                    var initialState = tunk.connectionApi.connectState(this, stateOptions_);
-
-                    for (var x in initialState) if (initialState.hasOwnProperty(x)) {
-                        Vue.util.defineReactive(this, x, initialState[x]);
-                    }
-
                 }
 
                 if (this.$options.actions) {
 
-                    var actionOptions = this.$options.actions, actionOptions_ = {};
+                    var actionOptions = this.$options.actions, tmp;
 
                     for (var x in actionOptions) if (actionOptions.hasOwnProperty(x)) {
                         if (typeof actionOptions[x] === 'string' && actionOptions[x].indexOf('.') > -1) {
-                            actionOptions_[x] = actionOptions[x].split('.');
+                            tmp = actionOptions[x].split('.');
+                            tunk.connection.action(this, x, tmp[0], tmp[1]);
                         } else {
-                            throw 'the action option should has dot between module name and action name:' + x + ':' + actionOptions[x];
+                            throw 'the action option should has dot between module name and action name ' + x + ':' + actionOptions[x];
                         }
                     }
-
-                    tunk.connectionApi.connectActions(this, actionOptions_);
                 }
 
-                tunk.connectionApi.setDispatchMethod(this, 'dispatch', function (dispatch) {
-                    return function (name) {
-                        if (typeof name !== 'string' || name.indexOf('.') === -1) throw 'the first argument should has dot between module name and action name: ' + name;
-                        name = name.split('.');
-                        dispatch(name[0], name[1], Array.prototype.slice.call(arguments, 1));
-                    };
-                });
+
+                this.dispatch = function(actionPath){
+                    if (typeof actionPath !== 'string' || actionPath.indexOf('.') === -1) throw 'the first argument should has dot between module name and action name: ' + actionPath;
+                    actionPath = actionPath.split('.');
+                    tunk.connection.dispatch(actionPath[0], actionPath[1], Array.prototype.slice.call(arguments, 1));
+                }
+
             },
 
 
             beforeDestroy: function () {
-                if (this.$options.stateOptions_) {
-                    tunk.connectionApi.disconnect(this, this.$options.stateOptions_);
-                }
+                tunk.connection.clean(this);
             },
         });
     }
