03abfdd3db13b4bba16ab7024b98517d1fc2c7e2
steven03.chen <steven03.chen@vipshop.com>
2017-11-30 17:20:15 +0800

    添加connect代码

--- tunk-vue.js
+++ tunk-vue.js
@@ -1,22 +1,83 @@
 (function () {
 
+    var connections = [];
+
+
+
     function tunkVue(utils) {
         var tunk = this;
+
+        var connect = {
+            state: function (targetObject, propName, statePath) {
+                if (!statePath[0] || !utils.modules[statePath[0]]) throw '[tunk]:unknown module name:' + statePath[0];
+                connections[statePath[0]] = connections[statePath[0]] || [];
+                connections[statePath[0]].push({
+                    comp: targetObject,
+                    propName: propName,
+                    statePath: statePath,
+                });
+                targetObject._tunkOptions_ = targetObject._tunkOptions_ || {};
+                targetObject._tunkOptions_[propName] = statePath;
+                //返回组件默认数据
+                return utils.hooks.getValueFromStore(statePath, utils.modules[statePath[0]].options);
+            },
+            action: function (target, propName, moduleName, actionName) {
+                target[propName] = function () {
+                    utils.dispatchAction(moduleName, actionName, arguments)
+                };
+            },
+
+            dispatch: function (target, dispatchName, handle, options) {
+                target[dispatchName] = handle(utils.dispatchAction);
+            },
+            
+            getModule: function (moduleName) {
+                if (!utils.modules[moduleName]) throw '[tunk]:unknown module name ' + moduleName;
+                return utils.modules[moduleName];
+            },
+        };
+
         tunk.install = function (Vue) {
-            utils.hook('updateComponentState', function (origin) {
-                return function (targetObject, stateName, newValue, options) {
-                    if (targetObject.$options.beforeStateChange)
-                        targetObject.$options.beforeStateChange.call(targetObject, stateName, newValue);
-                    targetObject[stateName] = newValue;
-                    origin(targetObject, stateName, newValue, options);
+            utils.hook('store', function (origin) {
+                return function (state, newState, options) {
+                    var pipes = connections[options.moduleName],
+                        changedFields = Object.keys(newState),
+                        statePath;
+
+                    origin(state, newState, options);
+
+                    setTimeout(function () {
+                        if (pipes && pipes.length) for (var i = 0, l = pipes.length; i < l; i++) if (pipes[i]) {
+                            statePath = pipes[i].statePath;
+                            // 只更新 changedFields 字段
+                            if (statePath[1] && changedFields.indexOf(statePath[1]) === -1) continue;
+                            //减少克隆次数，分发出去到达 View 的数据用同一个副本，减少调用 hooks.getValueFromStore
+                            ((targetObject, propName, newValue, options) => {
+                                if (targetObject.$options.beforeStateChange)
+                                    targetObject.$options.beforeStateChange.call(targetObject, statePath, newValue);
+                                targetObject[propName] = newValue;
+                            })(pipes[i].comp, pipes[i].propName, utils.hooks.getValueFromStore(statePath, options), options);
+                        }
+                    });
                 }
             });
+
 
             Vue.mixin({
                 init: init,
                 beforeCreate: init, // for Vue2.0 
                 beforeDestroy: function () {
-                    utils.connect.clean(this);
+                    if (this._tunkOptions_) {
+                        var stateOption = this._tunkOptions_;
+                        var tmp;
+                        for (var x in stateOption) {
+                            tmp = [];
+                            for (var i = 0, l = connections[stateOption[x][0]].length; i < l; i++) {
+                                if (connections[stateOption[x][0]][i].comp !== this) tmp.push(connections[stateOption[x][0]][i]);
+                            }
+                            connections[stateOption[x][0]] = tmp;
+                        }
+                    }
                 },
             });
 
@@ -25,11 +86,11 @@
                     var stateOptions = this.$options.state;
                     if (stateOptions.constructor === Array) {
                         for (var i = 0; i < stateOptions.length; i++) {
-                            Vue.util.defineReactive(this, stateOptions[i], utils.connect.state(this, stateOptions[i], stateOptions[i].split('.')));
+                            Vue.util.defineReactive(this, stateOptions[i], connect.state(this, stateOptions[i], stateOptions[i].split('.')));
                         }
                     } else
                         for (var x in stateOptions) if (stateOptions.hasOwnProperty(x)) {
-                            Vue.util.defineReactive(this, x, utils.connect.state(this, x, stateOptions[x].split('.')));
+                            Vue.util.defineReactive(this, x, connect.state(this, x, stateOptions[x].split('.')));
                         }
                 }
 
@@ -37,10 +98,10 @@
                     var actionOptions = this.$options.actions, tmp;
                     if (actionOptions.constructor === Array) {
                         for (var i = 0, x = actionOptions[0]; i < actionOptions.length; i++ , x = actionOptions[i]) {
-                            var proto = utils.connect.getModule(x).__proto__,
+                            var proto = connect.getModule(x).__proto__,
                                 protoNames = Object.getOwnPropertyNames(proto);
                             for (var i = 0, y = protoNames[0]; i < protoNames.length; i++ , y = protoNames[i]) if (proto[y].options && proto[y].options.isAction) {
-                                utils.connect.action(this, x + '_' + y, x, y);
+                                connect.action(this, x + '_' + y, x, y);
                             }
                         }
                     } else
@@ -48,18 +109,18 @@
                             if (typeof actionOptions[x] === 'string')
                                 if (actionOptions[x].indexOf('.') > -1) {
                                     tmp = actionOptions[x].split('.');
-                                    utils.connect.action(this, x, tmp[0], tmp[1]);
+                                    connect.action(this, x, tmp[0], tmp[1]);
                                 } else {
-                                    var proto = utils.connect.getModule(actionOptions[x]).__proto__,
+                                    var proto = connect.getModule(actionOptions[x]).__proto__,
                                         protoNames = Object.getOwnPropertyNames(proto);
                                     for (var i = 0, y = protoNames[0]; i < protoNames.length; i++ , y = protoNames[i]) if (proto[y].options && proto[y].options.isAction) {
-                                        utils.connect.action(this, x + '_' + y, actionOptions[x], y);
+                                        connect.action(this, x + '_' + y, actionOptions[x], y);
                                     }
                                 }
                         }
                 }
 
-                utils.connect.dispatch(this, 'dispatch', function (dispatch) {
+                connect.dispatch(this, 'dispatch', function (dispatch) {
                     return function (actionPath) {
                         if (typeof actionPath !== 'string' || actionPath.indexOf('.') === -1) throw '[TUNK-VUE]:the first argument should has dot between module name and action name: ' + actionPath;
                         actionPath = actionPath.split('.');
